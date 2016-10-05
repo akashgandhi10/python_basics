43e209462dc22805d9cef17d52bf9078fe39e0c3
Akash Gandhi <akashgandhi10@gmail.com>
2016-10-04 19:58:16 -0500

    excercises for functions in python

--- functions.py
+++ functions.py
@@ -668,7 +668,7 @@ def palin(s):
 		
 	return True
 
-print palin("Go hang a salami I'm a lasagna hog.")"""
+print palin("Go hang a salami I'm a lasagna hog.")
 
 
 
@@ -678,11 +678,317 @@ print palin("Go hang a salami I'm a lasagna hog.")"""
 def pangram(s):
 	l = "abcdefghijklmnopqrstuvwxyz"
 	for i in l:
-		if i not in l:
+		if i not in s:
 			return False
 	return True
 
-print pangram("The quick brown fox jumps over the lazy")
+print pangram("The quick brown fox jumps over the lazy dog.")
+
+
+def pangram(s):
+	d = {}
+	s = s.lower()
+	for i in s:
+		if i.isalpha() and i not in d:
+			d[i] = 0
+	if len(d.keys()) == 26:
+		return True
+	return False
+
+print pangram("The quick brown fox jumps over the lazy dog.")
+
+'''"99 Bottles of Beer" is a traditional song in the United States and Canada. It is popular to sing on long trips, as it has a very repetitive format which is easy to memorize, and can take a long time to sing. The song's simple lyrics are as follows:
+
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down, pass it around, 98 bottles of beer on the wall.
+
+The same verse is repeated, each time with one fewer bottle. The song is completed when the singer or singers reach zero.
+
+Your task here is write a Python program capable of generating all the verses of the song.'''
+
+n = 99
+def load():
+
+	n = 99
+	for i in range(0,99):
+		print ("%s bottles of beer on the wall, %s bottles of beer.\nTake one down, pass it around, %s bottles of beer on the wall." %(n,n,n-1))
+		n -= 1
+
+load()
+
+
+#Write a function char_freq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Python dictionary. Try it with something like char_freq("abbabcbdbabdbdbabababcbcbab").
+
+
+
+def freq(s):
+	
+	s = s.lower()
+	d = {}
+	count = 0
+	for i in s:
+		if i.isalpha() and i not in d:
+			d[i] = 1
+		elif i.isalpha() and i in d:
+			d[i] += 1
+		
+	
+	print d.keys()
+	print d.values()
+	print len(d.keys())
+	return d
+
+print freq("abbabcbdbabdbdbabababcbcbab")
+
+
+'''In cryptography, a Caesar cipher is a very simple encryption techniques in which each letter in the plain text is replaced by a letter some fixed number of positions down the alphabet. For example, with a shift of 3, A would be replaced by D, B would become E, and so on. The method is named after Julius Caesar, who used it to communicate with his generals. ROT-13 ("rotate by 13 places") is a widely used example of a Caesar cipher where the shift is 13. In Python, the key for ROT-13 may be represented by means of the following dictionary:
+
+key = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 
+       'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 
+       'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k',
+       'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 
+       'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 
+       'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 
+       'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+Your task in this exercise is to implement an encoder/decoder of ROT-13. Once you're done, you will be able to read the following secret message:
+
+   Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!
+Note that since English has 26 characters, your ROT-13 program will be able to both encode and decode texts written in English.'''
+
+def rot_13(s):
+	key = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 
+       'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 
+       'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k',
+       'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 
+       'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 
+       'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 
+       'W':'J', 'X':'K', 'Y':'L', 'Z':'M', ' ': ' ', '?':'?', '!':'!'}
+	s = s.lower()
+	s1 = ""
+	for i in s:
+		#if i.isalpha():
+		i = key[i]
+		s1 += i
+	return s1
+
+print rot_13("Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!")
+
+#Define a simple "spelling correction" function correct() that takes a string and sees to it that 1) two or more occurrences of the space character is compressed into one, and 2) inserts an extra space after a period if the period is directly followed by a letter. E.g. correct("This   is  very funny  and    cool.Indeed!") should return "This is very funny and cool. Indeed!" Tip: Use regular expressions!
+
+
+'''The third person singular verb form in English is distinguished by the suffix -s, which is added to the stem of the infinitive form: run -> runs. A simple set of rules can be given as follows:
+
+If the verb ends in y, remove it and add ies
+If the verb ends in o, ch, s, sh, x or z, add es
+By default just add s
+Your task in this exercise is to define a function make_3sg_form() which given a verb in infinitive form returns its third person singular form. Test your function with words like try, brush, run and fix. Note however that the rules must be regarded as heuristic, in the sense that you must not expect them to work for all cases. Tip: Check out the string method endswith().'''
+
+def third(s):
+	
+
+	if s.endswith("y"):
+		s1 = s[:-1]
+		s1 = s1 + "ies"
+		return s1
+	
+	elif s.endswith("o") or s.endswith("s") or s.endswith("x") or s.endswith("z"):
+		
+		s = s + "es"
+		return s
+	
+	elif s.endswith("ch") or s.endswith("sh"):
+		
+		s = s + "es"
+		return s
+	
+	else:
+		s = s + "s"
+		return s
+print third("play")
+print third("brush")
+print third("run")
+print third("fix")
+print third("help")
+
+'''In English, the present participle is formed by adding the suffix -ing to the infinite form: go -> going. A simple set of heuristic rules can be given as follows:
+
+If the verb ends in e, drop the e and add ing (if not exception: be, see, flee, knee, etc.)
+If the verb ends in ie, change ie to y and add ing
+For words consisting of consonant-vowel-consonant, double the final letter before adding ing
+By default just add ing
+Your task in this exercise is to define a function make_ing_form() which given a verb in infinitive form returns its present participle form. Test your function with words such as lie, see, move and hug. However, you must not expect such simple rules to work for all cases.'''
+
+
+		
+	
+
+def ing(s):
+	l1 = "aeiou"
+	
+	if s.endswith("ie"):
+		
+		
+		return s[:-2] + "ying"
+
+
+	elif s.endswith("e") and s != "be" and s != "see" and s != "flee" and s != "knee":
+		
+		return s[:-1] + "ing"
+	
+		
+	elif s[-1] not in l1 and s[-2] in l1 and s[-3] not in l1:
+		
+	
+		return s + s[-1] + "ing"
+	
+	else:
+	
+		return s + "ing"
+		
+print ing('be')
+print ing('lie')
+print ing('see')
+print ing('move')
+print ing('hug')
+print ing("be")
+print ing("stop")
+print ing("play")"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

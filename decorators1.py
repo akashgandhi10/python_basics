3636f7ff6e83f7b80a5b20d4fed9dfeb3d8a3a48
Akash Gandhi <akashgandhi10@gmail.com>
2016-11-13 21:20:06 -0600

    decorators startup
    Signed-off-by: Akash Gandhi <akashgandhi10@gmail.com>

---
+++ decorators1.py
@@ -0,0 +1,32 @@
+def func():
+    return 1
+
+print func()
+
+x = func
+
+print x()
+
+s = "this is global variable"
+
+def func1():
+    print locals()
+
+func1()
+print globals()
+print globals()["s"]
+
+def hello(name = "Akash"):
+    return "Hello " + name
+
+print hello()
+
+greet = hello
+
+print greet()
+
+del hello
+
+#NameError print hello() it's not longer exists but greet() does
+
+print greet()
